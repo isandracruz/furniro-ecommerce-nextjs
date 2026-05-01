9fe90c9a5602c6868157796697588a320e0d8133
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-05-01 15:33:21 -0400

    feat: replace header placeholder with Header component in MainLayout

--- components/layout/MainLayout/MainLayout.tsx
+++ components/layout/MainLayout/MainLayout.tsx
@@ -1,12 +1,13 @@
+import Header from "../Header/Header";
+
 interface MainLayoutProps {
   children: React.ReactNode;
 }
+
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <div>
-        <h1>Header</h1>
-      </div>
+      <Header />
 
       <main className="flex-1 flex flex-col">
         {children}
