99fe123d5efe83fe23cc941f36c454e7397d328a
Isandra Cruz Diaz <isandra.cruz.2017@gmail.com>
2026-06-16 15:57:04 -0400

    feat: add About, Contact, and Shop pages with basic layout

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,8 +1,8 @@
-export default function Home() {
+export default function Shop() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
       <main className="flex flex-1 w-full max-h-3xl flex-col items-center justify-between bg-white sm:items-start">
-        Home
+        Shop
       </main>
     </div>
   );
